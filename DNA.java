df318b2e5fc1f6dc0179e40e73fcec52fe3416d6
[Ravi Prasad] <[ravikrp22@gmail.com]>
2020-11-02 11:53:52 +0530

    [Added Questions pdf and 1st Answer]

---
+++ DNA.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class DNA{
+    public static void main(String[]args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Give Input");
+        String s=scan.nextLine();
+        int n=s.length();
+        String a="";
+        for(int i=0;i<n;i++)
+        {
+            if(s.charAt(i)=='G') {
+            	a+="C";
+            }
+            if(s.charAt(i)=='C') {
+            	a+="G";
+            }
+            if(s.charAt(i)=='T') {
+            	a+="A";
+            }
+            if(s.charAt(i)=='A') {
+            	a+="U";
+            }
+    }
+        System.out.print(a);
+    }
+}
