be30952283a81f0efd0e0b2533e53b9fb98412a6
[Ravi Prasad] <[ravikrp22@gmail.com]>
2020-11-06 20:17:50 +0530

    [Updated Q3 Solution]

--- Question3.java
+++ Question3.java
@@ -1,24 +1,31 @@
-import java.util.*;
 public class Question3 {
 
 	public static void main(String[] args) {
-		Scanner scan=new Scanner(System.in);
-		System.out.println("Enter a Number");
-		int n=scan.nextInt();
-		System.out.print(n+"->");
-		
-		while(n!=1) {
-			if(n%2==0) {
-				n=n/2;
-				System.out.print(n+"->");
-			}
-			else {
-				n=(3*n+1);
-				System.out.print(n+"->");
-			}
-		}
-
-		
+		long a=0,b=0;
+        int count=0;
+        for(long i=2;i<1000000;i++)
+        {
+       	 count=1;
+       	 long n=i;
+       	 
+       	 while(n!=1)
+       	 {
+       		 if(n%2==0)
+       			 n/=2;
+       		 else
+       			 n=3*n+1;
+       		 count++;
+       		 
+       	 }
+       	 
+       	 if(count>a&&n==1)
+       	 {
+       		 a=count;
+       		 
+       		 b=i;
+       	 }
+        }
+        System.out.println(b);
 	}
 
 }
